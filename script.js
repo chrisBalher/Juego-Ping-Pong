af574c5b5af6a95cde8741dd746d2c809b47f007
chrisBalher <c.baldeon.h@gmail.com>
2020-12-01 10:23:20 -0500

    posicionadcanvas width/2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const context = canvas.getContext('2d')
 const width = 500;
 const height = 600;
 const screenWidth = window.screen.width;
-const canvasPosition = screenWidth / 2 - width ;
+const canvasPosition = screenWidth / 2 - width/2 ;
 const isMobile = window.matchMedia('(max-width: 600px)');
 const gameOverEl = document.createElement('div');
 
